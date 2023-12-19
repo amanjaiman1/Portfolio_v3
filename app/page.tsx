ad98ce11e89e143ee172cc8d89900641e0a748dc
AMAN JAIMAN <77478827+amanjaiman1@users.noreply.github.com>
2023-12-19 23:07:00 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>Hii, I'm starting soon</div>
+      <div>Hii, I'll start soon </div>
     </main>
   )
 }
