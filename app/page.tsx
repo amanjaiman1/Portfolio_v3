bdb18795928b3738dcd5fb67117b0b065888ce8e
AMAN JAIMAN <77478827+amanjaiman1@users.noreply.github.com>
2024-01-16 22:47:06 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>Hii, I'm starting soon</div>
+      <div>Hii, I think i gonna be too late</div>
     </main>
   )
 }
